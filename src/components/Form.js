1a0cb7c1ce3223fa422a22a5c7d6ab17c29024a2
Ashok Matwa <ashokmatwa003@gmail.com>
2023-06-01 10:56:12 +0530

    second

--- src/components/Form.js
+++ src/components/Form.js
@@ -26,31 +26,7 @@ export default function Form() {
 
         handleNumber();
         handleString();
-
-       /* let newNumber = document.getElementById('number').value;
-
-        // if(typeof newNumber==="string") alert("not a number");
-        //if(newNumber==="string") alert("not a number");
-        //if(number==0) alert("not a number");
-        // if(typeof newNumber === "number")
-
-        if(!isNaN(newNumber))
-            setNumber(newNumber);
-        else alert("not a number");
-
-        let newString = document.getElementById('string').value;
-        //if(typeof newString === "string")
-        if(isNaN(newString))
-            setString(newString);
-        else alert("not a string");
-
-        console.log(typeof newNumber);
-        console.log(typeof newString); */
     }
-
-    // const myFunction = () => {
-    //     console.log("function")
-    // }
 
     // function handleForm(e){ // to prevent in loading the page 
     //     e.preventDefault();
@@ -61,13 +37,11 @@ export default function Form() {
     <div>
       {/* <form onSubmit={handleForm}> */}
         <label for="number">input a number</label>
-        {/* <input type='number' id='number' name='number'></input> */}
         <input type='text' id='number' name='number'></input>
         {/* <input type='text' id='number' name='number' onChange={handleNumber}></input> */}
 
         <br></br>
         <label for="string">input a string</label>
-        {/* <input type='string' id='string' name='string'></input> */}
         <input type='text' id='string' name='string'></input>
         {/* <input type='text' id='string' name='string' onChange={handleString}></input> */}
 
@@ -83,8 +57,3 @@ export default function Form() {
     </div>
   );
 }
-
-// Form.propTypes = {
-//     number : PropTypes.number,
-//     string : PropTypes.string
-// }
