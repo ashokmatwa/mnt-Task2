70ea9a7c56976fb6bf0e278f323b2be089432e23
Ashok Matwa <ashokmatwa003@gmail.com>
2023-06-01 12:39:35 +0530

    third

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,26 +6,28 @@ export default function Form() {
     const [number, setNumber] = useState(" ");
     const [string, setString] = useState(" ");
 
-    function handleNumber(){
+    function handleNumber(e){
         console.log("number");
-        let newNumber = document.getElementById('number').value;
-        if(!isNaN(newNumber))
+        let newNumber = e.target.value;
+        let regex = /^[0-9]+$/;
+        if(newNumber.match(regex))
+        // if(!isNaN(newNumber))
             setNumber(newNumber);
         else alert("not a number");
     }
-    function handleString(){
+    function handleString(e){
         console.log("string");
-        let newString = document.getElementById('string').value;
-        if(isNaN(newString))
+        let newString = e.target.value;
+        let regex = /^[a-zA-Z]+$/;
+
+        if(newString.match(regex))
+        // if(isNaN(newString))
             setString(newString);
         else alert("not a string");
     }
     
     function myFunction(){
         console.log("function");
-
-        handleNumber();
-        handleString();
     }
 
     // function handleForm(e){ // to prevent in loading the page 
@@ -37,13 +39,11 @@ export default function Form() {
     <div>
       {/* <form onSubmit={handleForm}> */}
         <label for="number">input a number</label>
-        <input type='text' id='number' name='number'></input>
-        {/* <input type='text' id='number' name='number' onChange={handleNumber}></input> */}
+        <input type='text' id='number' name='number' onChange={handleNumber}></input>
 
         <br></br>
         <label for="string">input a string</label>
-        <input type='text' id='string' name='string'></input>
-        {/* <input type='text' id='string' name='string' onChange={handleString}></input> */}
+        <input type='text' id='string' name='string' onChange={handleString}></input>
 
         <br></br>
         <input type="submit" onClick={myFunction}></input>
